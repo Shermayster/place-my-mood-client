474f06dc412d9b81d7909a04f2f4a8a7410b8d68
pavelshermayster <sherm84@gmail.com>
2018-07-28 14:16:35 +0300

    feat: added Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Link } from "react-router-dom";
 import './App.css';
-
 import logo from './logo.svg';
+
 
 class App extends React.Component {
   public render() {
@@ -10,6 +11,21 @@ class App extends React.Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
+          <Router>
+          <div>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/topics">Topics</Link>
+              </li>
+            </ul>
+            </div>
+          </Router>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
